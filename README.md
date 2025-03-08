5f771e8403f85ac9f173cea33f0b070a61b73d91
Your Name <you@example.com>
2025-03-08 13:40:40 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1> Proyecto amigo-secreto</h1>
+
+- Challenger 
---
+++ app.js
@@ -0,0 +1,61 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombres = [];
+let largoArray             = 0;
+let indiceArray            = 0;
+let nombreEscrito          = '';
+let nombreAmigoSecreto         = '' ;
+
+
+
+function agregarAmigo() 
+{
+    
+    nombreEscrito = document.querySelector('#amigo').value;
+    
+    if (nombreEscrito != '') { 
+    
+            let li = document.createElement("li");
+            
+            if (listaNombres.includes(nombreEscrito))
+            {
+                alert("Ya existe");
+            }else{
+                listaNombres.push(nombreEscrito) 
+
+                li.textContent = nombreEscrito;
+
+            document.getElementById("listaAmigos").appendChild(li);
+            limpiarCaja();
+            
+            }
+        }else 
+        {
+           alert ("Caja vacia");
+
+        }
+}
+
+function sortearAmigo()
+{
+    largoArray = listaNombres.length ; 
+    let numeroGenerado =  Math.floor(Math.random()*largoArray)+1;
+    nombreAmigoSecreto = listaNombres [numeroGenerado-1]
+    asignarTextoDocumento('#nomAmigoSecreto',`Tu amigo secreto es ${nombreAmigoSecreto}`);
+
+}
+
+function limpiarCaja ()
+{
+
+  let valorCaja = document.querySelector('#amigo');
+  valorCaja.value = '';
+}
+
+function asignarTextoDocumento (elemento, texto)
+{
+       
+    let elementoHTML = document.querySelector(elemento);
+       
+    elementoHTML.innerHTML =texto;
+
+}
